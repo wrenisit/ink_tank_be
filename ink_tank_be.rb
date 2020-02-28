d909ce472ea8542b74dbaa1b37ff3ffddbf37819
Wren Steitle <lauren.steitle@gmail.com>
2020-02-28 08:18:56 -0700

    Create bad advice!

--- ink_tank_be.rb
+++ ink_tank_be.rb
@@ -1,19 +1,10 @@
 require 'sinatra/base'
-require 'stripe'
 require 'pry'
 
 class InkTankBE < Sinatra::Base
-  post '/payments' do
-    # param_amount = params[:amount]
-    # param_source = params[:source]
-    binding.pry
-    Stripe.api_key = ENV['STRIPE_KEY']
-    charge = Stripe::Charge.create({amount: '20000', currency: 'usd', source: 'tok_mastercard'})
+  get '/advice' do
+    advice = ["yes", "no", "but, why?", "your mother will hate it.", "fine.", "maybe?"]
+    result = advice.sample
+    result.to_json
   end
 end
-
-# FE
-# conn = Faraday.new(myherokuapp.url)
-# conn.post('/payments') do |f|
-#   f.body = {all the info goes here
-#   }.to_json
--- spec/ink_tank_spec.rb
+++ spec/ink_tank_spec.rb
@@ -7,28 +7,18 @@ require 'pry'
 
 RSpec.describe 'The Ink Tank' do
   describe InkTankBE do
+  def app
+    Sinatra::Application
+  end
   include Rack::Test::Methods
     let(:ink_tank_be) { InkTankBE.new }
 
-    context "POST to /payments" do
-      let(:response) { post "/payments" }
-
+    context "GET to /advice" do
+      let(:response) { get "/advice" }
+      binding.pry
       it "returns status 200 OK" do
         expect(response.status).to eq 200
       end
     end
-  end
-  def app
-    Sinatra::Application
-  end
-
-  it "gets stripe" do
-    data = {
-      'name' => "Mallory Archer",
-      'amount' => "20000",
-      'source' => "tok_mastercard"
-    }
-    post '/payments', data.to_json
-    expect(last_response).to be_ok
   end
 end
